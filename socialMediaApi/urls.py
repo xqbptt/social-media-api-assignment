0677dd6dabe138f5a1f7d5df1a51763ef03718ca
shiv <xqbptt@gmail.com>
2022-01-16 02:33:15 +0530

    fixed collectstatic error

--- socialMediaApi/urls.py
+++ socialMediaApi/urls.py
@@ -38,6 +38,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('Authentication.urls')),
     path('api/', include('socialMedia.urls')),
+    path('api/api.json/', schema_view.without_ui(cache_timeout=0),
+         name='schema-swagger-ui'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',cache_timeout=0), name='schema-redoc'),
 ]
